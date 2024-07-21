86abca222f88d290892741a2b9203877b092f803
Stavros Charitos <polikasxaritos@gmail.com>
2024-07-21 20:00:59 +0100

    created .NET MAUI app, learned basic architecture of .NET MAUI, defined shared UI for the platforms supported by .NET MAUI, deployed a .NET MAUI app from VS, used .NET MAUI to access platforms APIs, Runs on windows, and android, can access a phone dialer through android phones feature

---
+++ TestPage.cs
@@ -0,0 +1,49 @@
+﻿
+namespace MyMauiApp
+{
+    public partial class TestPage : ContentPage
+    {
+        int count = 0;
+
+        Label counterLabel;
+
+        public TestPage()
+        {
+            var myScrollView = new ScrollView();
+
+            var myStackLayout = new VerticalStackLayout
+            {
+                BackgroundColor = Color.FromRgba("7B3F00")
+            };
+            myScrollView.Content = myStackLayout;
+
+            counterLabel = new Label
+            {
+                Text = "Test Page Current count: 0",
+                FontSize = 18,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            myStackLayout.Children.Add(counterLabel);
+
+            var myButton = new Button
+            {
+                Text = "Click me",
+                HorizontalOptions = LayoutOptions.Center
+            };
+            myStackLayout.Children.Add(myButton);
+
+            myButton.Clicked += OnCounterClicked!;
+
+            this.Content = myScrollView;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            counterLabel.Text = $"Test Page Current count: {count}";
+
+            SemanticScreenReader.Announce(counterLabel.Text);
+        }
+    }
+}
